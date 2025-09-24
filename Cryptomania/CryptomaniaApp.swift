861a134d95a4af01b4b5d3b22c785fc7cb4e4fd6
RoyDMAP <roy.dimapilis@yahoo.com>
2025-09-24 16:37:16 -0700

    Initial Commit

---
+++ Cryptomania/CryptomaniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptomaniaApp.swift
+//  Cryptomania
+//
+//  Created by Roy Dimapilis on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptomaniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
